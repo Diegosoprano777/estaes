81f769e65626fb00e19ff8dac81be4b7ec9a74fb
Diegosoprano777 <hiphopclan99@gmail.com>
2026-02-20 11:43:16 -0500

    Se agrega formulario Bootstrap con validación y descarga JSON

--- src/app/pages/nosotros/nosotros.ts
+++ src/app/pages/nosotros/nosotros.ts
@@ -1,13 +1,45 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-nosotros',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    FormsModule
+  ],
   templateUrl: './nosotros.html',
   styleUrls: ['./nosotros.css']
 })
 export class NosotrosComponent {
 
+  formulario = {
+    nombre: '',
+    correo: '',
+    mensaje: ''
+  };
+
+  descargarJSON(form: NgForm) {
+
+    // Si el formulario no es válido, no hace nada
+    if (form.invalid) {
+      Object.values(form.controls).forEach(control => control.markAsTouched());
+      return;
+    }
+
+    const dataStr = JSON.stringify(this.formulario, null, 2);
+    const blob = new Blob([dataStr], { type: 'application/json' });
+    const url = window.URL.createObjectURL(blob);
+
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'formulario.json';
+    a.click();
+
+    window.URL.revokeObjectURL(url);
+
+    // reset formulario después de descargar
+    form.resetForm();
+  }
 }
